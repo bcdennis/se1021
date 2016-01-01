b48f1407064a4083dbc04eb423a240cc60271a8f
bcdennis@gmail.com <bcdennis@gmail.com>
2016-01-01 11:46:25 -0600

    adding stubs for lecture 08

---
+++ src/lecture08/JFrameDemo.java
@@ -0,0 +1,18 @@
+package lecture08;
+
+/**
+ * JFrame.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * more JFrame.
+ *
+ * @author Brad Dennis
+ * Created:  1/4/2015
+ * Modified: --
+ */
+public class JFrameDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lecture08/JOptionPaneDemo.java
@@ -0,0 +1,18 @@
+package lecture08;
+
+/**
+ * JOptionPane.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * more JOptionPane.
+ *
+ * @author Brad Dennis
+ * Created:  1/4/2015
+ * Modified: --
+ */
+public class JOptionPaneDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
