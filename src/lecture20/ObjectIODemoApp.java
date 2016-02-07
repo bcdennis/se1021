78b3bd9a6a7a11f4ada6c1d25c75b5d3e8404223
bcdennis@gmail.com <bcdennis@gmail.com>
2016-02-07 12:46:18 -0600

    prepping for lecture 20 object i/o workshop

--- src/lecture20/ObjectIODemoApp.java
+++ src/lecture20/ObjectIODemoApp.java
@@ -7,44 +7,28 @@
  */
 package lecture20;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 /**
  * Demonstration app for Object I/O.
  */
 public class ObjectIODemoApp {
 
     public void go() {
-        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("person.bin"))) {
-
-            Person p = new Person("Dennis", "Brad", "bcdennis@gmail.com");
-            stream.writeObject(p);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream("person.bin"))) {
-            Person p = (Person) stream.readObject();
-            System.out.println(p);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        writePersonToFile();
+        readPersonFromFile();
     }
+
 
     public static void main(String[] args) {
         ObjectIODemoApp app = new ObjectIODemoApp();
         app.go();
+    }
+
+
+    private void writePersonToFile() {
+        //TODO complete this method.
+    }
+
+    private void readPersonFromFile() {
+        //TODO complete this method.
     }
 }
